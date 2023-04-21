639e48e0233d4a2deeb157f1df8f7ff3f74c4f05
Richard Chimama <rich.chime@gmail.com>
2023-04-21 02:05:44 +0200

    configured the main file

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,25 +1,68 @@
-import React from 'react'
-import * as S from "./styled"
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import * as S from "./styled";
+import { useNavigate } from "react-router-dom";
+import { TbReport, TbReportSearch } from "react-icons/tb";
+import { CgFileDocument } from "react-icons/cg";
+import { motion } from "framer-motion";
 
 const Menu = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [screenWidth, setScrrenWidth] = useState(0);
+
+  const handleScreenResize = () => {
+    const GET_WINDOW_WIDTH = window.screen.width;
+    setScrrenWidth(GET_WINDOW_WIDTH);
+  };
+
+  useEffect(()=>{
+    setScrrenWidth(window.screen.width);
+  },[])
+
+  window.onresize = handleScreenResize;
+
   return (
     <>
-    <S.MenuComponent>
-        <h1>MAIN MENU</h1>
-        <S.Menu>
-            <button onClick={()=> navigate("/fiche")}>FICHE DE CONSULTATION</button>
+      <S.H1>MAIN MENU</S.H1>
+      <S.MenuComponent className="menu">
+        <motion.div
+            whileHover={{ scale: 1.1}}
+        >
+        <S.Menu className="menu-button" onClick={() => navigate("/fiche")}>
+          {screenWidth > 420 && (
+            <CgFileDocument className="icon-menu" color={"white"} size={110} />
+          )}
+          <div className="label-menu" >
+            FICHE DE CONSULTATION
+          </div>
         </S.Menu>
-        <S.Menu>
-            <button onClick={()=>  navigate("/recherche")}>RECHERCHE PATIENT</button>
+        </motion.div>
+        <motion.div
+            whileHover={{ scale: 1.1}}
+        >
+        <S.Menu className="menu-button" onClick={() => navigate("/recherche")} >
+          {screenWidth > 420 && (
+            <TbReportSearch className="icon-menu" color={"white"} size={110} />
+          )}
+          <div className="label-menu" >
+            RECHERCHE PATIENT
+          </div>
         </S.Menu>
-        <S.Menu>
-            <button onClick={()=> navigate("/rapport")}>RAPPORT</button>
+        </motion.div>
+        <motion.div
+            whileHover={{ scale: 1.1}}
+        >
+        <S.Menu className="menu-button" onClick={() => navigate("/rapport")}>
+          {screenWidth > 420 && (
+            <TbReport className="icon-menu" color={"white"} size={110} />
+          )}
+          <div className="label-menu" >
+            RAPPORT
+          </div>
         </S.Menu>
-    </S.MenuComponent>
+        </motion.div>
+      </S.MenuComponent>
     </>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
--- src/Components/Menu/styled.ts
+++ src/Components/Menu/styled.ts
@@ -1,23 +1,78 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Menu = styled.div`
-    & > button {
-        background-color: green;
-        color: white;
-    }
-    
-    @media (max-width: 320){
-        & > button{
-            width: 100%;
-            color: black;
-        }
-    }
-`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 200px;
+  background-color: #228558;
+  border-radius: 10px;
+  width: 100%;
+  min-width: 135px;
+  border: 1px solid #228558;
+  padding-top: 10px;
+  cursor: pointer;
 
-export const MenuComponent = styled.div`
+  & > .label-menu {
+    position: absolute;
+    background-color: #fff;
+    color: #228558;
+    font-size: 16px;
+    border: none;
+    height: 70px;
+    width: 100%;  
+    bottom: 0px;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    text-align: center;
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 2rem;
+
+    @media screen and (max-width: 420px) {
+      position: unset;
+      background-color: #228558;
+      color: #fff;
+      height: 100%;
+      bottom: none;
+      border-radius: 10px;
+    }
+
+    & > .icon-menu{
+        display: none;
+        color: red;
+       
+        @media screen and (max-width: 420px) {
+        }        
+    }
+  }
+`;
+
+export const MenuComponent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+  width: 80%;
+  margin-right: 10%;
+  margin-left: 10%;
+
+  & > div{
+    width: 100%;
+  }
+
+  @media screen and (max-width: 420px) {
+    min-width: none;
+    & > div{
+    height: 40px;
+
+    &> div{
+        height: 40px;
+  }
+  }
+`;
+
+export const H1 = styled.h1`
+    text-align: center;
+    color: #228558;
 `
