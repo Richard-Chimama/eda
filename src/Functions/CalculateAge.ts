8097f47911db026c90e113bd5ea9d27721a05d46
Richard Chimama <rich.chime@gmail.com>
2023-05-23 18:10:52 +0200

    fixed the calculate function

--- src/Functions/CalculateAge.ts
+++ src/Functions/CalculateAge.ts
@@ -1,13 +1,11 @@
-interface Props{
-    date_of_birth: string | number | Date
-}
+
 /**
  * This function calculate the age of a person
  * @param date_of_birth is the date of the birth
  * @returns the age of of the person
  */
 
-const CalculateAge=({date_of_birth}:Props)=>{
+const CalculateAge=(date_of_birth:any)=>{
   const date = new Date(Date.now());
   const currentYear = date.getFullYear()
   const DoB = new Date(date_of_birth).getFullYear()
